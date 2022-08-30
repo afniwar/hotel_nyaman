47100be7a746d5fd74299e5fd9ab7bb5ac28a0f1
afniwar <diway.afniwar@gmail.com>
2022-08-31 05:42:17 +0700

    menambahkan gambar bottom

--- lib/page/page_splash.dart
+++ lib/page/page_splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_nyaman/tema.dart';
 
 class PageSplash extends StatelessWidget {
   const PageSplash({Key? key}) : super(key: key);
@@ -6,11 +7,63 @@ class PageSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('assets/hotel_logo.png'),
-          Image.asset('assets/logo.png'),
-        ],
+      body: SafeArea(
+        child: Stack(children: [
+          Align(
+            child: Image.asset(
+              "assets/Rectangle95.png",
+            ),
+            alignment: Alignment.bottomCenter,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/hotel_logo.png'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Temukan Hotel Terbaik\nBersama Kami HOTELIN",
+                  style: hitammediumtext.copyWith(fontSize: 24),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Cari Hotel dan tempat menginap terbaik\nHOTELIN Ajaa..",
+                  style: abutext.copyWith(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: warnaHijau,
+                    minimumSize: Size(210, 50),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(17),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Book Now',
+                    style: putihtext,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
--- lib/tema.dart
+++ lib/tema.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Color warnaUngu = const Color(0xff5843BE);
+Color warnaHijau = const Color(0xff36ab9c);
 Color warnaOrange = const Color(0xffFF9376);
 Color warnaPutih = Colors.white;
 Color warnaHitam = Colors.black;
 Color warnaAbu = const Color(0xff82868E);
 
-TextStyle hitamboldtext = GoogleFonts.poppins(
-  fontWeight: FontWeight.bold,
+TextStyle hitammediumtext = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
   color: warnaHitam,
 );
 
-TextStyle putihboldtext = GoogleFonts.poppins(
-  fontWeight: FontWeight.bold,
+TextStyle putihmediumtext = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: warnaPutih,
+);
+
+TextStyle putihtext = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
   color: warnaPutih,
 );
 
 TextStyle ungutext = GoogleFonts.poppins(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w500,
   color: warnaUngu,
 );
 
